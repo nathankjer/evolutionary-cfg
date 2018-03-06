02bd0447c31910f5f1ee843736fb0c9992f714ae
Nathan Kjer <nathan.kjer@rutgers.edu>
2018-03-06 18:12:59 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from optimizers import EvolutionaryCFG
+
+def evaluate(expression):
+    return math.fabs(eval(expression))
+
+def main():
+    symbols = ['x','y','z','T','I','S']
+    grammar = {
+        'x':[str(float(x)) for x in xrange(1,10)],
+        'y':[str(float(x)) for x in xrange(1,10)],
+        'z':[str(float(x)) for x in xrange(1,10)],
+        'T':['x','y','z'],
+        'I':['I+I','I-I','I*I','I/I','(I)'] + ['T']*5,
+        'S':['I+I','I-I','I*I','I/I']
+    }
+
+    optimizer = EvolutionaryCFG(symbols, grammar, evaluate, population_size=300, num_attributes=7)
+    optimizer.evolve(generations=4)
+
+if __name__ == '__main__':
+    main()
